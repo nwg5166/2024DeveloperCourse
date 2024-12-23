8013277cc886c4945c9cbd0440e1a992459d88dd
nwg5166 <nwg5166@outlook.com>
2024-12-23 22:28:32 +0000

    end of day

---
+++ 18. Advanced Javascript and DOM Manipulation/index.js
@@ -0,0 +1,53 @@
+
+for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var crash1 = new Audio("sounds/crash.mp3");
+            crash1.play();
+            break;
+
+        case "k":
+            var kick1 = new Audio("sounds/kick-bass.mp3");
+            kick1.play();
+            break;
+
+        case "l":
+            var snare1 = new Audio("sounds/snare.mp3");
+            snare1.play();
+            break;
+
+        default: console.log(button);
+            break;
+    }
+});
+
+}
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//    audio.play();
